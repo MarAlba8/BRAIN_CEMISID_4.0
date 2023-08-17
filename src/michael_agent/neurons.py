781eb82b0eb9ee0481574032c5d00dd7b1ed231b
Michael Eyzell <MichaelEyzell92@gmail.com>
2023-08-17 17:10:44 -0300

    update class 17/08/2023

--- src/michael_agent/neurons.py
+++ src/michael_agent/neurons.py
@@ -8,10 +8,10 @@ class Neurons():
         self.tmp = None
 
     def set_event(self, string):
-        pattenr, event = string.split(":")
+        pattern, event = string.split(":")
         
         if event not in self.learned_neurons:
-            self.neuron_to_learn[event]=[len(self.learned_neurons),self.learned_neurons[pattenr][1]]
+            self.neuron_to_learn[event]=[len(self.learned_neurons),self.learned_neurons[pattern][1]]
             return self.neuron_to_learn[event]
         else:
             self.tmp = self.learned_neurons[event]
@@ -19,8 +19,9 @@ class Neurons():
         
     def set_event_bce(self, event, bce):
         if event not in self.learned_neurons:
-            self.neuron_to_learn[event]=[len(self.learned_neurons),BCE().none()]
-            self.update_neuron_to_learn(bce)
+            self.learned_neurons[event]=[len(self.learned_neurons),bce]
+            #self.neuron_to_learn[event]=[len(self.learned_neurons),bce]
+            #self.update_neuron_to_learn(bce)
 
         return self.learned_neurons[event]
 
@@ -28,15 +29,27 @@ class Neurons():
         if bool(self.neuron_to_learn):
             event, value = self.neuron_to_learn.popitem()
             id_neurona = len(self.learned_neurons)
-            self.learned_neurons[event]=[id_neurona,(bce+value[1])//2]
+            self.learned_neurons[event]=[id_neurona,(bce.average(value[1]))]
             return self.learned_neurons[event]
         else:
             return self.tmp 
 
-    def init_patterns(self,arr_patternes_bce):
-        for pattern_bce in arr_patternes_bce:
-            self.learned_neurons[pattern_bce[0]]=[len(self.learned_neurons),pattern_bce[1]]
+    def init_patterns(self,arr_patterns_bce):
+        #print(arr_patterns_bce)
+        list_return = []
+        for pattern_bce in arr_patterns_bce:
+            data = self.set_event_bce(pattern_bce[0],pattern_bce[1])
+            data_return = (data[0],pattern_bce[0],data[1])
+            list_return.append(data_return)
+            #list_id_neurons.append(id_neurons)
+            #self.learned_neurons[pattern_bce[0]]=[id_neurons,pattern_bce[1]]
+        return list_return
+        
 
     def get_neurons(self):
         return self.learned_neurons
 
+    def reset(self):
+        self.learned_neurons = {}
+        self.neuron_to_learn = {}
+        
