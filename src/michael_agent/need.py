ab9227e837f2db491bc6e6a0d6f3f0b7e2cc5bb5
Michael Eyzell <MichaelEyzell92@gmail.com>
2023-08-17 17:13:08 -0300

    update class 17/08/2023

--- src/michael_agent/need.py
+++ src/michael_agent/need.py
@@ -1,11 +1,12 @@
 import random
 import numpy as np
 
-#random.seed(2)
-
 LEN_DEGREE=4
 
 class Need():
+
+    sign_str={0:"+",1:"-"}
+
     def __init__(self, sign=0, degree=0, len_degree=LEN_DEGREE):
 
         self.len_sign=2
@@ -16,7 +17,17 @@ class Need():
         else:
             self.state = np.array([sign,degree], dtype=np.int_) 
         
-        self.sign_str={0:"+",1:"-"}
+         
+
+    def set(self, num, len_degree=LEN_DEGREE):
+
+        self.len_sign=2
+        self.len_degree=len_degree
+        sign= 0 if num >=0 else 1
+        degree=abs(num) if abs(num)<=self.len_degree else self.len_degree
+
+        self.state = np.array([sign,degree], dtype=np.int_) 
+        return self
 
     def __str__(self):
         if self.state[0]!=None and self.state[1]!=None:
@@ -30,7 +41,6 @@ class Need():
         else:
             return "None"
         
-    
     def __add__(self, other):
         if other.state[0]==None or other.state[1]==None:
             return Need(self.state[0],self.state[1])
@@ -40,30 +50,46 @@ class Need():
         if self.state[1] == 0 and other.state[1] == 0:
             return Need(self.state[0],self.state[1])
         
+        # sign1= [1,-1]
+        # degree1= self.state[1]*sign1[self.state[0]]
+        # degree2= other.state[1]*sign1[other.state[0]]
+
+        # ret_val=degree1+degree2
+        # if ret_val ==0:
+        #     ret_sign=self.state[0]
+        # elif ret_val >0:
+        #     ret_sign=0
+        #     ret_val=min(ret_val,self.len_degree)
+        # else:
+        #     ret_sign=1
+        #     ret_val=min(abs(ret_val),self.len_degree)
+        # return Need(ret_sign,ret_val)
+
+        #self.state[1]+other.state[1]
         elif self.state[0]==0:
-            if other.state[0] == 0:
-                sign = 0
-                degree = min(self.state[1]+other.state[1],self.len_degree)
-            else:
-                if self.state[1] >= other.state[1]:
-                    sign = 0
-                    degree = self.state[1]-other.state[1]
-                else:
-                    sign = 1
-                    degree = abs(self.state[1]-other.state[1])
+           if other.state[0] == 0:
+               sign = 0
+               degree = self.state[1]+other.state[1]
+           else:
+               if self.state[1] >= other.state[1]:
+                   sign = 0
+                   degree = self.state[1]-other.state[1]
+               else:
+                   sign = 1
+                   degree = abs(self.state[1]-other.state[1])
         else:
-            if other.state[0] == 1:
-                sign = 1
-                degree = min(self.state[1]+other.state[1],self.len_degree)
-            else:
-                if self.state[1] >= other.state[1]:
-                    sign = 1
-                    degree = self.state[1]-other.state[1]
-                else:
-                    sign = 0
-                    degree = abs(self.state[1]-other.state[1])
+           if other.state[0] == 1:
+               sign = 1
+               degree = self.state[1]+other.state[1]
+           else:
+               if self.state[1] >= other.state[1]:
+                   sign = 1
+                   degree = self.state[1]-other.state[1]
+               else:
+                   sign = 0
+                   degree = abs(self.state[1]-other.state[1])
                     
-        return Need(sign,degree)
+        return Need(sign,min(degree,self.len_degree))
     
     def __sub__(self, other):	#To get called on subtraction operation using - operator.  
         if other.state[0] == None and other.state[1] == None:
@@ -75,27 +101,42 @@ class Need():
         return self+Need(sign,other.state[1])
 
     def __mul__(self, escalar):	#To get called on multiplication operation using * operator.
-        if self.state[0] == 0 and escalar >= 0 or self.state[0] == 1 and escalar < 0:
-            return Need(0,self.state[1]*escalar)+Need(0,0)
+        if self.state[1]==0 or escalar ==0:
+            sign=self.state[0]
+        elif self.state[0] == 0 and escalar > 0:
+            sign=0
+        elif self.state[0] == 1 and escalar < 0:
+            sign=0
         else:
-            return Need(1,self.state[1]*escalar)+Need(0,0)
+            sign=1
+        return Need(sign,self.state[1]*abs(escalar))+Need(0,0)
 
     def __truediv__(self, escalar):	#To get called on division operation using / operator.
-        if self.state[0] == 0 and escalar > 0 or self.state[0] == 1 and escalar < 0:
-            return Need(0,self.state[1]/escalar)+Need(0,0)
+        if self.state[1]==0:
+            sign=self.state[0]
+        elif self.state[0] == 0 and escalar > 0:
+            sign=0
+        elif self.state[0] == 1 and escalar < 0:
+            sign=0
         else:
-            return Need(1,self.state[1]/escalar)+Need(0,0)
+            sign=1
+        return Need(sign,self.state[1]/abs(escalar))+Need(0,0)
 
     def __floordiv__(self, escalar):	#To get called on division operation using // operator.
-        if self.state[0] == 0 and escalar > 0 or self.state[0] == 1 and escalar < 0:
-            return Need(0,self.state[1]//escalar)+Need(0,0)
+        if self.state[1]==0:
+            sign=self.state[0]
+        elif self.state[0] == 0 and escalar > 0:
+            sign=0
+        elif self.state[0] == 1 and escalar < 0:
+            sign=0
         else:
-            return Need(1,self.state[1]//escalar)+Need(0,0)
+            sign=1
+        return Need(sign,self.state[1]//abs(escalar))+Need(0,0)
 
     def __lt__(self, other):    #To get called on comparison using < operator.
-        if self.state[0]==None and self.state[1]==None and other.state[0]!=None and other.state[1]!=None:
+        if (self.state[0]==None and self.state[1]==None) and (other.state[0]!=None and other.state[1]!=None):
             return True
-        elif other.state[0]==None and other.state[1]==None and self.state[0]!=None and self.state[1]!=None:
+        elif (other.state[0]==None and other.state[1]==None) and (self.state[0]!=None and self.state[1]!=None):
             return False
         elif self.state[0]==0 and other.state[0]==0:
             return self.state[1] < other.state[1]
@@ -115,6 +156,8 @@ class Need():
     def __ne__(self, other):    #To get called on comparison using != operator.
         if self == other:
             return False
+        else:
+            return True
 
     def __le__(self, other):    #To get called on comparison using <= operator.
         if self == other:
@@ -125,7 +168,9 @@ class Need():
             return False
 
     def __gt__(self, other):    #To get called on comparison using > operator.
-        if self < other:
+        if self == other:
+            return False
+        elif self < other:
             return False
         else:
             return True
@@ -142,6 +187,7 @@ class Need():
         sign=random.randint(0,self.len_sign-1)
         degree=random.randint(0,self.len_degree-1)
         self.state=np.array([sign,degree], dtype=np.int_)
+        return self
 
     def sample(self):
         sign=random.randint(0,self.len_sign-1)
@@ -154,20 +200,35 @@ class Need():
         return Need(sign,degree)
 
     def zero(self):
-        return Need(0,0)
+        sign=0
+        degree=0
+        self.state=np.array([sign,degree], dtype=np.int_)
+        return self
 
     def none(self):
-        return Need(None,None)
+        sign=None
+        degree=None
+        self.state=[sign,degree]
+        return self
 
-#ejemplos de otros metodos magicos
-#__add__(self, other)	To get called on add operation using + operator
-#__sub__(self, other)	To get called on subtraction operation using - operator.  
-#__lt__(self, other)	To get called on comparison using < operator.
-#__le__(self, other)	To get called on comparison using <= operator.
-#__ge__(self, other)	To get called on comparison using >= operator.
-#__gt__(self, other)	To get called on comparison using > operator.
-#__eq__(self, other)	To get called on comparison using == operator.
-#__ne__(self, other)	To get called on comparison using != operator.
-#__mul__(self, other)	To get called on multiplication operation using * operator.
-#__floordiv__(self, other)	To get called on floor division operation using // operator.
-#__truediv__(self, other)	To get called on division operation using / operator.
+    def average(self, other):
+        if self.state[0]==0:
+            item1=self.state[1]
+        else:
+            item1=self.state[1]*(-1)
+
+        if other.state[0]==0:
+            item2=other.state[1]
+        else:
+            item2=other.state[1]*(-1)
+
+        avg=(item1+item2)//2
+
+        if avg==0:
+            return Need(self.state[0],abs(avg))+Need(0,0)
+        elif avg<0:
+            return Need(1,abs(avg))+Need(0,0)
+        else:
+            return Need(0,abs(avg))+Need(1,0)
+
+        
--- src/michael_agent/sensory_nn.py
+++ src/michael_agent/sensory_nn.py
@@ -11,13 +11,31 @@ class Sensory_NN():
         return self.neurons.get_neurons()
 
     def set_event_bce(self,event,bce):
-        return [self.neurons.set_event_bce(event,bce),self.id]
+        if not event.endswith("_"+self.id):
+            event += "_"+self.id
+
+        return self.neurons.set_event_bce(event,bce)+[self.id]
 
     def set_event(self,event):
-        return [self.neurons.set_event(event),self.id]
+        pattern = event.split(':')[0]
+        if not pattern.endswith("_"+self.id):
+            pattern += "_"+self.id
+        
+        event_split = event.split(':')[1]
+        if not event_split.endswith("_"+self.id):
+            event_split += "_"+self.id      
+        
+        pattern_event = pattern + ":" + event_split
+        return self.neurons.set_event(pattern_event)+[self.id]
 
     def update_neuron(self, bce):
-        return [self.neurons.update_neuron_to_learn(bce),self.id]
+        return self.neurons.update_neuron_to_learn(bce)+[self.id]
     
-    def init_patterns(self, arr_patternes_bce):
-        self.neurons.init_patterns(arr_patternes_bce)
+    def init_patterns(self, arr_patterns_bce):
+        result_list=[]
+        result_list = [(pattern,bce) if pattern.endswith("_"+self.id) else (pattern+ "_"+self.id, bce) for pattern, bce in arr_patterns_bce]
+        return self.neurons.init_patterns(result_list)
+        
+    def reset(self):
+        self.neurons.reset()
+        
